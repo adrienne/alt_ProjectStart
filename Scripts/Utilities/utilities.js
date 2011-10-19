bd27299a55d1b31b9e03e3b32e1d91669dea4f70
Adrienne L. Travis <adrienne@utilitarienne.com>
2011-10-19 16:47:06 -0500

    playing with isType more

--- Scripts/Utilities/utilities.js
+++ Scripts/Utilities/utilities.js
@@ -2,27 +2,45 @@
 ** --  A recursive function to return a string of random characters.                           -- ** 
 ** ---------------------------------------------------------------------------------------------- */
 
-function RandomChars(length,current) {
-    var allowedchars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghiklmnopqrstuvwxyz";
-    current = current || '';
-    return length ? RandomChars(--length,allowedchars.charAt(Math.floor(Math.random() * allowedchars.length)) + current) : current;
-    }
-    
-(function(global) {
-  Object.toType = function(obj) {
-    var key;
-    // If the object is null, return "Null" (IE <= 8)
-    return obj === null ? { itemType : "Null" }
-      // If the object is undefined, return "Undefined" (IE <= 8)
-      : obj == null ? { itemType : "Undefined" }
-      // If the object is the global object, return "Global"
-      : obj === global ? { itemType : "Global" }
-      // Otherwise return the XXXXX part of the full [object XXXXX] value, from
-      // cache if possible.
-      : {
-        itemType : ({}).toString.call(obj).match(/\s([a-z|A-Z]+)/)[1],
-        objectType : (obj.constructor && (obj.constructor.toString().match(/\s*function (.*)\(/))) ? obj.constructor.toString().match(/\s*function (.*)\(/)[1] : undefined,
-        domType : (obj.jquery) ? 'jQuery' : ((obj.nodeType) ? 'DOM' : undefined)
-        }
-  };
-}(this));
+var ALTutils = ALTutils || {};  // Declare a namespace^
+
+(function(global) { // Begin anonymous function, passing in a context (which will be the global object)
+
+    var that = this;    // Minimize inconsistent behavior of this
+
+/* -- ALTutils.RandomChars^ (length NUMBER , current STRING) ------------------------------------ **
+** --    returns: STRING                                                                       -- **
+** -- A recursive function to return a string of random characters.                            -- ** 
+** -- Pass the number of characters you want to length; do not pass a value to current.        -- **
+** ---------------------------------------------------------------------------------------------- */
+    ALTutils.RandomChars = function (length,current) {
+        var allowedchars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghiklmnopqrstuvwxyz";
+        current = current || '';
+        return length ? this.RandomChars(--length,allowedchars.charAt(Math.floor(Math.random() * allowedchars.length)) + current) : current;
+        };
+
+/* --  ALTutils.isType^ (obj OBJECT) ------------------------------------------------------------ **
+** --     returns: OBJECT { itemType: STRING, objectType: STRING(*), domType: STRING(*)        -- **
+** --                                                     (*): may also be UNDEFINED!          -- **
+** --  A function to return more useful type information about a variable (replaces typeOf)    -- ** 
+** --  Pass in an object or variable.                                                          -- ** 
+** ---------------------------------------------------------------------------------------------- */
+    ALTutils.isType = function(obj) {
+        // If the object is null, return "Null" (IE <= 8)
+        return obj === null ? { itemType : "Null" }
+          // If the object is undefined, return "Undefined" (IE <= 8)
+          : obj == null ? { itemType : "Undefined" }
+          // If the object is the global object, return "Global"
+          : obj === global ? { itemType : "Global" }
+          // Otherwise return the XXXXX part of the full [object XXXXX] value, from
+          // cache if possible.
+          : {
+            itemType : ({}).toString.call(obj).match(/\s([a-z|A-Z]+)/)[1],
+            /* TODO: there's a bug in objectType */
+            objectType : (obj.constructor && (obj.constructor.toString().match(/\s*function (.*)\(/))) ? obj.constructor.toString().match(/\s*function (.*)\(/)[1] : undefined,
+            domType : (obj.jquery) ? 'jQuery' : ((obj.nodeType) ? 'DOM' : undefined)
+            }
+        };
+        
+        
+    }(this));   // End our anonymous function, passing in the global object (as this)
